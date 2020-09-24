d8d75dffcc682e9a64be7f015f8e6590ef444c84
Raghad Alrasheed <raghad@MacBook-Pro-alkhas-b-Raghad.local>
2020-09-24 14:09:36 +0300

    2nd js task done

---
+++ story.js
@@ -0,0 +1,12 @@
+let num = 10;
+let num2 = 4;
+let op = "-";
+
+if (op == "+"){
+console.log(num+num2)}
+else if (op == "-"){
+console.log(num-num2)}
+else if (op == "*"){
+console.log(num*num2)}
+else if (op == "/"){
+console.log(num/num2)}
